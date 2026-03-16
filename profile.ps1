d5d579322c7573adf6c8833d7c292fae9fecde9f
Jamesafluke <42627144+Jamesafluke@users.noreply.github.com>
2026-03-16 16:50:14 -0600

    Refactor profile.ps1 by removing note functions
    Removed several note-related functions and paths, added reboot alias.

--- profile.ps1
+++ profile.ps1
@@ -1,40 +1,8 @@
 
-
-$code = "C:\Code\"
-$notes = "C:\Users\v-fluckigerj\OneDrive\Notes\"
-$xboxDaily = "C:\Users\v-fluckigerj\OneDrive\Notes\Xbox.txt"
-$cSharp = "C:\Users\v-fluckigerj\OneDrive\Notes\C#"
-$generalXbox = "C:\Users\v-fluckigerj\OneDrive\Notes\XboxGeneral"
-
-function notes{
-	Set-Location $notes
-	ls
-}
-
-Function note{
-    Nvim-Location $Notes
-}
-
-Function cnote{
-    Nvim-Location $cSharp
-}
-
-Function general{
-    Nvim-Location $generalXbox
-}
-
-
-Set-Alias -Name n nvim
 Set-Alias -Name ll "Get-ChildItem -Force"
-Set-Alias -Name t -Value terraform
-Set-Alias -Name k -Value kubectl
-Set-Alias -Name g -Value git
+Set-Alias -Name reboot "Restart-Computer -f"
 Set-Alias -Name ub3 -Value "C:\PersonalMyCode\UpdateBudget3\main.ps1"
-Set-Alias -Name vi -Value nvim
-Set-Alias -Name v -Value nvim
-Set-Alias -Name vim -Value nvim
 Set-Alias -Name touch -Value New-Item
-
 
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json' | Invoke-Expression
 

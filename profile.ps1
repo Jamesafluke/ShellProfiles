7374b7d49739df1be4e38ebe184a3486a69c1dfd
Jamesafluke <42627144+Jamesafluke@users.noreply.github.com>
2026-03-19 15:00:13 -0600

    Replace alias 'll' with function to list files
    Replaced alias 'll' with a function to list items excluding system files.

--- profile.ps1
+++ profile.ps1
@@ -1,12 +1,7 @@
-
-Set-Alias -Name ll "Get-ChildItem -Force"
 Set-Alias -Name reboot "Restart-Computer -f"
 Set-Alias -Name ub3 -Value "C:\PersonalMyCode\UpdateBudget3\main.ps1"
 Set-Alias -Name touch -Value New-Item
 
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json' | Invoke-Expression
-
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
+function ll {
+    Get-ChildItem -Force | Where-Object { $_.Attributes -notlike "*System*" }
+}
